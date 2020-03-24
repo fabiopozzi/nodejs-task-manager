d77a1175346667fc0b702eb67ffa13de2ee50a4c
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-24 15:33:42 +0100

    Exploring mongodb ObjectIDs

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,13 @@
 // CRUD exploration
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -13,15 +16,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Fabio',
-    //     age: 36
-    // }, (error, result) => {
-    //    if (error) {
-    //        console.log('Unable to insert user')
-    //    }
-    //    console.log(result.ops)
-    // })
+    db.collection('users').insertOne({
+        _id: id,
+        name: 'Pino',
+        age: 40
+    }, (error, result) => {
+       if (error) {
+           console.log('Unable to insert user')
+       }
+       console.log(result.ops)
+    })
 
     // db.collection('users').insertMany([
     //     {
@@ -37,23 +41,23 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     //         }
     //         console.log(result.ops)
     // })
-    db.collection('tasks').insertMany([
-        {
-            description: 'Descrizione numero 1',
-            completed: true
-        },
-        {
-            description: 'Come se fosse antani',
-            completed: false
-        },
-        {
-            description: 'Lezione numero 3',
-            completed: true
-        }
-    ], (error, result) => {
-        if (error) {
-            console.log('Unable to insert tasks')
-        }
-        console.log(result.ops)
-    })
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Descrizione numero 1',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'Come se fosse antani',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'Lezione numero 3',
+    //         completed: true
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert tasks')
+    //     }
+    //     console.log(result.ops)
+    // })
 })
