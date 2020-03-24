7c588ec32e66a1fb7445281c4c1fa528fd2e45f2
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-24 15:36:36 +0100

    Difference between the binary and string representation of ObjectIDs

--- mongodb.js
+++ mongodb.js
@@ -6,8 +6,10 @@ const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
+console.log(id.id)
+console.log(id.id.length)
+console.log(id.toHexString())
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -16,16 +18,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Pino',
-        age: 40
-    }, (error, result) => {
-       if (error) {
-           console.log('Unable to insert user')
-       }
-       console.log(result.ops)
-    })
+    // db.collection('users').insertOne({
+    //     _id: id,
+    //     name: 'Pino',
+    //     age: 40
+    // }, (error, result) => {
+    //    if (error) {
+    //        console.log('Unable to insert user')
+    //    }
+    //    console.log(result.ops)
+    // })
 
     // db.collection('users').insertMany([
     //     {
