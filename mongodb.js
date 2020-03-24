e6253ca84c59b2ba90e69fdc055b07f99610bf7e
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-24 09:21:26 +0100

    First challenge on document insertion completed.

--- mongodb.js
+++ mongodb.js
@@ -6,15 +6,54 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database')
     }
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Fabio',
-        age: 36
+    // db.collection('users').insertOne({
+    //     name: 'Fabio',
+    //     age: 36
+    // }, (error, result) => {
+    //    if (error) {
+    //        console.log('Unable to insert user')
+    //    }
+    //    console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //         if (error) {
+    //             console.log('Unable to insert documents')
+    //         }
+    //         console.log(result.ops)
+    // })
+    db.collection('tasks').insertMany([
+        {
+            description: 'Descrizione numero 1',
+            completed: true
+        },
+        {
+            description: 'Come se fosse antani',
+            completed: false
+        },
+        {
+            description: 'Lezione numero 3',
+            completed: true
+        }
+    ], (error, result) => {
+        if (error) {
+            console.log('Unable to insert tasks')
+        }
+        console.log(result.ops)
     })
 })
