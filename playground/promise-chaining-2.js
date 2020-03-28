93f073b0ea2969ed6b813e0dc5e13050208741d3
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-28 08:22:02 +0100

    Refactor playground using async/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,10 +2,22 @@ require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
 
-Task.findByIdAndDelete('5e7ca8e68abc55780eb74bd1').then(() => {
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log('incomplete tasks: ', result)
+// Task.findByIdAndDelete('5e7ca8e68abc55780eb74bd1').then(() => {
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log('incomplete tasks: ', result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5e7ef9d18ecc4eed79465a44').then((count) => {
+    console.log('Count vale ', count)
 }).catch((e) => {
-    console.log(e)
+    console.log('Errore ', e)
 })
