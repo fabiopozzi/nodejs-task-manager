b9f184ec33ff71765665794c7e58bc40b0f3a6d8
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-30 16:30:28 +0200

    First experiments with express middleware. Challenge solved (trivial manteinance_mode handling)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,25 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+const manteinance_mode = false
+
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.status(500).send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// disable everything when manteinance mode
+app.use((req, res, next) => {
+    if(manteinance_mode) {
+        res.status(503).send('The website is under manteinance, please try again later.')
+    } else{
+        next()
+    }
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -13,6 +32,8 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ', port)
 })
+
+
 
 const jwt = require('jsonwebtoken')
 
