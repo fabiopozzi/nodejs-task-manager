370c80f05ebbe87ff1d4f529619a13ff4c122e3b
Fabio Pozzi <fabio@fabiopozzi.net>
2020-04-03 01:30:09 +0200

    Add timestamps to models. Refactor read tasks to allow filtering only completed/non-completed tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -34,13 +34,19 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed=true     /tasks?completed=false
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
     try {
-        const tasks = await Task.find({ owner: req.user._id})
-        // alternative
-        // req.user.populate('tasks').execPopulate()
-        // res.send(req.user.tasks)
-        res.send(tasks)
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
