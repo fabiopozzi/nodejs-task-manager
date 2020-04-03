b6f1a2fd7b57ace1ee16595421c327ae40863ac1
Fabio Pozzi <fabio@fabiopozzi.net>
2020-04-03 10:15:29 +0200

    Add limit and skip query options to implement pagination.

--- src/routers/task.js
+++ src/routers/task.js
@@ -35,6 +35,7 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 // GET /tasks?completed=true     /tasks?completed=false
+// GET /tasks?limit=10&skip=0
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
     if (req.query.completed) {
@@ -44,7 +45,11 @@ router.get('/tasks', auth, async (req, res) => {
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
